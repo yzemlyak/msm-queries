970b90b4643b235d898a7d4268f161c40433fc96
yzemlyak <yzemlyak@gmail.com>
2025-05-06 01:28:53 +0000

    Fixed an error

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
   get("/directors", {:controller=>"directors", :action=> "index"})
-  get("/directors/:the_id", {:controller=>"directors", :action=> "show"})
-  get("")
+  get("/director/:the_id", {:controller=>"directors", :action=> "show"})
+  get("/movies", {:controller=>"movies", :action => "index"})
+  get("/movies/:the_id", {:controller=>"movies", :action=> "show"})
 end
