f782aae1337b49410dbc0fe82e010cc908665cb0
yzemlyak <yzemlyak@gmail.com>
2025-05-06 01:53:56 +0000

    Fixed bug

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
   get("/directors", {:controller=>"directors", :action=> "index"})
-  get("/director/:the_id", {:controller=>"directors", :action=> "show"})
+  get("/directors/:the_id", {:controller=>"directors", :action=> "show"})
   get("/movies", {:controller=>"movies", :action => "index"})
   get("/movies/:the_id", {:controller=>"movies", :action=> "show"})
 end
