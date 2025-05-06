1a616611d4a84b9e9eeb157c100ad43ee088ccde
yzemlyak <yzemlyak@gmail.com>
2025-05-06 02:37:24 +0000

    all but puzzle

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get("/directors/:the_id", {:controller=>"directors", :action=> "show"})
   get("/movies", {:controller=>"movies", :action => "index"})
   get("/movies/:the_id", {:controller=>"movies", :action=> "show"})
+  get("/actors", {:controller=>"actors", :action=> "index"})
+  get("/actors/:the_id", {:controller=>"actors", :action=> "show"})
 end
